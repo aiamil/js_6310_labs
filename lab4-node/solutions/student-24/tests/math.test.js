103cb53f5eaf5a3d2cb7ba381391ec4ca388e9b1
aiamil <isadyrttinova@bk.ru>
2025-11-24 21:06:14 +0300

    git commit -m "Lab 4 solution by student 24"

---
+++ lab4-node/solutions/student-24/tests/math.test.js
@@ -0,0 +1,16 @@
+import * as math from '../src/utils';
+import { describe, test, expect } from '@jest/globals';
+
+describe('Math module', () => {
+  test('sums two numbers correctly', () => {
+    expect(math.sum(2, 3)).toBe(5);
+  });
+  
+  test('multiplies two numbers correctly', () => {
+    expect(math.multiply(2, 3)).toBe(6);
+  });    
+  
+  test('sum handles negative numbers', () => {
+    expect(math.sum(-1, -2)).toBe(-3);
+  });
+});
